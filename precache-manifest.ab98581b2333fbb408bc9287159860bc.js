7a00643397b139686ddbb3e6519eb82cabf1f65d
httpsbao <50363688+httpsbao@users.noreply.github.com>
2020-10-15 07:12:23 +0000

    Deploying to gh-pages from master f3e439355d262910b2f0443de53992c6b9b0e773

---
+++ precache-manifest.ab98581b2333fbb408bc9287159860bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfa2f86886a1195735623bba993fd42",
+    "url": "/Action-test/index.html"
+  },
+  {
+    "revision": "e2d1c1d4d438bed050fe",
+    "url": "/Action-test/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "809b13e8048b4cfc0c89",
+    "url": "/Action-test/static/js/2.50d12b3e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Action-test/static/js/2.50d12b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d1c1d4d438bed050fe",
+    "url": "/Action-test/static/js/main.72c48e93.chunk.js"
+  },
+  {
+    "revision": "2de7d4b0cf3167d0335b",
+    "url": "/Action-test/static/js/runtime-main.bab0b0f7.js"
+  }
+]);
